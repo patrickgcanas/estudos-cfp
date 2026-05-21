6211424c53620e3234657e643e6366ff136b7ab9
Patrick Gare Canãs <patrickg.canas@gmail.com>
2026-05-21 16:18:01 -0300

    Fix UUID format for Supabase sync + bump SW cache to v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cfp-estudos-v3';
+const CACHE = 'cfp-estudos-v4';
 const CORE = ['./', './index.html', './data.js', './manifest.json', './icons/icon-192.svg', './icons/icon-512.svg'];
 
 self.addEventListener('install', e => {
